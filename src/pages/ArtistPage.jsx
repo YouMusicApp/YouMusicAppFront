b64fa256da2d6304ad1edd82ef836996431fbc09
juanfco86 <juanfco86@gmail.com>
2022-12-12 15:52:56 +0100

    artist page similar songs and genres

--- src/pages/ArtistPage.jsx
+++ src/pages/ArtistPage.jsx
@@ -3,6 +3,7 @@ import { BsFillPlayFill } from 'react-icons/bs';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import AlbumSlider from '../Components/Slider/AlbumSlider/AlbumSlider';
+import Slider from '../Components/Slider/Slider';
 import { TopInfoArtist } from '../Components/TopInfo/TopInfoArtist/TopInfoArtist';
 import { setPlayer } from '../helpers/functions/setPlayer';
 
@@ -15,10 +16,13 @@ export const ArtistPage = () => {
     const albums = useSelector(state => state.albumSlice);
     const album = albums.list.filter((album) => album.artist === artist.name);
 
+    let listGenreArtist = [];
+    const genreSong = artist.genres.map((genre) => listGenreArtist = [...listGenreArtist, tracks.list.filter((track) => track.genre === genre)]);
+    const similarSongs = listGenreArtist.flat()
 
     return (
         <>
-            <TopInfoArtist data={artist}/>
+            <TopInfoArtist data={artist} />
             <div className='container mb-4'>
                 <table className="table">
                     <thead className="thead-dark">
@@ -30,23 +34,23 @@ export const ArtistPage = () => {
                         </tr>
                     </thead>
                     <tbody>
-                    {
-                        tracksArtist.map((track) => {
-                            return (
-                            <tr className='cursor-pointer'>
-                                <td onClick={() => setPlayer(track)} className='cursor-pointer tdhover'><BsFillPlayFill /></td>
-                                <td>{track.name}</td>
-                                <td>{track.artist}</td>
-                                <td>{track.genre}</td>
-                            </tr>
-                            )
-                        })
-                    }
+                        {
+                            tracksArtist.map((track) => {
+                                return (
+                                    <tr className='cursor-pointer'>
+                                        <td onClick={() => setPlayer(track)} className='cursor-pointer tdhover'><BsFillPlayFill /></td>
+                                        <td>{track.name}</td>
+                                        <td>{track.artist}</td>
+                                        <td>{track.genre}</td>
+                                    </tr>
+                                )
+                            })
+                        }
                     </tbody>
                 </table>
             </div>
-            
-            <div className="container mt-2">
+
+            <div className="container mt-2 mb-4">
                 <AlbumSlider
                     slidesPerView={2}
                     size='small'
@@ -77,6 +81,77 @@ export const ArtistPage = () => {
                     }}
                 />
             </div>
+            <div className="container mt-2 mb-4">
+                <Slider
+                    slidesPerView={2}
+                    size='small'
+                    img='img__small'
+                    array={similarSongs}
+                    title='Similar songs'
+                    breakpoints={{
+                        600: {
+                            slidesPerView: 3,
+                            spaceBetween: 10,
+                        },
+                        768: {
+                            slidesPerView: 4,
+                            spaceBetween: 10,
+                        },
+                        992: {
+                            slidesPerView: 5,
+                            spaceBetween: 10,
+                        },
+                        1200: {
+                            slidesPerView: 7,
+                            spaceBetween: 10,
+                        },
+                        1400: {
+                            slidesPerView: 8,
+                            spaceBetween: 10,
+                        }
+                    }}
+                />
+            </div>
+            {
+                listGenreArtist.map((e) => {
+                    const capitalizeGenre = e[0].genre.charAt(0).toUpperCase() + e[0].genre.slice(1);
+
+                    return (
+                        <div className="container mt-2 mb-4">
+                            <Slider
+                                slidesPerView={2}
+                                size='small'
+                                img='img__small'
+                                array={e}
+                                title={capitalizeGenre}
+                                breakpoints={{
+                                    600: {
+                                        slidesPerView: 3,
+                                        spaceBetween: 10,
+                                    },
+                                    768: {
+                                        slidesPerView: 4,
+                                        spaceBetween: 10,
+                                    },
+                                    992: {
+                                        slidesPerView: 5,
+                                        spaceBetween: 10,
+                                    },
+                                    1200: {
+                                        slidesPerView: 7,
+                                        spaceBetween: 10,
+                                    },
+                                    1400: {
+                                        slidesPerView: 8,
+                                        spaceBetween: 10,
+                                    }
+                                }}
+                            />
+                        </div>
+                    )
+                })
+            }
+
         </>
     )
 }
