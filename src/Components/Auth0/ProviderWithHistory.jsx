54fea21f16080709cbdcb283fe43c39976e2390f
lidiettes <hello@lidiettes.com>
2023-01-10 11:26:37 +0100

    auth dev

--- src/Components/Auth0/ProviderWithHistory.jsx
+++ src/Components/Auth0/ProviderWithHistory.jsx
@@ -5,7 +5,7 @@ import { Auth0Provider } from '@auth0/auth0-react';
 const ProviderWithHistory = ({ children }) => {
     const domain = process.env.REACT_APP_AUTH0_DOMAIN;
     const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
-
+    const audience = process.env.REACT_APP_AUTH0_AUDIENCE;
     // const history = useHistory();
 
     // const onRedirectCallback = (appState) => {
@@ -18,6 +18,7 @@ const ProviderWithHistory = ({ children }) => {
             clientId={clientId}
             redirectUri={window.location.origin}
             // onRedirectCallback={onRedirectCallback}
+            audience={audience}
         >
             {children}
         </Auth0Provider>
